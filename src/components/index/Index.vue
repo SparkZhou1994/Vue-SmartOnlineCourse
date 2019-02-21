c14988f25339614b443ad2a99e9dc0524930d95e
SparkZhou <zhoujuhui@qq.com>
2019-02-21 18:25:56 +0800

    carousel image todo

---
+++ src/components/index/Index.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-container>
+    <el-header>
+      <IndexHeader></IndexHeader>
+    </el-header>
+    <el-main>
+      <el-carousel :interval="4000" type="card" height="200px">
+        <el-carousel-item v-for="item in carousel_img_src_list" :key="item.index">
+          <img :src="item.url"/>
+        </el-carousel-item>
+      </el-carousel>
+    </el-main>
+    <el-footer></el-footer>
+  </el-container>
+</template>
+
+<script>
+import IndexHeader from './IndexHeader'
+export default {
+  name: 'Index',
+  components: {IndexHeader},
+  data: function () {
+    return {
+      carousel_img_src_list: [
+        {url: '../../assets/image/index/index_ad_01.jpg', index: 1},
+        {url: '../../assets/image/index/index_ad_02.jpg', index: 2},
+        {url: '../../assets/image/index/index_ad_03.jpg', index: 3}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+</style>
---
+++ src/components/index/IndexHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-row align="middle">
+    <el-col :xs="4" :sm="2">
+      <a href="#"><img class="logo-image" src="../../assets/image/logo.png" height="60"></a>
+    </el-col>
+    <el-col :xs="20" :sm="22">
+      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+        <el-menu-item index="1"><a href="">首页</a></el-menu-item>
+        <el-menu-item index="2"><a href="">关键特征</a></el-menu-item>
+        <el-menu-item index="3"><a href="">如何使用</a></el-menu-item>
+        <el-menu-item index="4"><a href="">学校服务</a></el-menu-item>
+        <el-menu-item index="5"><a href="">用户反馈</a></el-menu-item>
+        <el-menu-item index="6"><a href="">登录</a></el-menu-item>
+      </el-menu>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexHeader',
+  data () {
+    return {
+      activeIndex: '1'
+    }
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  a:hover
+    text-decoration:none
+</style>
