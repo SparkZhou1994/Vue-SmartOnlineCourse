0b3c897eb1264c3f12b66d1eb87abd1588fb173c
SparkZhou <zhoujuhui@qq.com>
2019-02-22 00:03:10 +0800

    bootstrap4 row

--- src/components/index/Index.vue
+++ src/components/index/Index.vue
@@ -4,11 +4,28 @@
       <IndexHeader></IndexHeader>
     </el-header>
     <el-main>
-      <el-carousel :interval="4000" type="card" height="200px">
+      <el-carousel :interval="4000" arrow="always" :height="316/936*screenWidth-50">
         <el-carousel-item v-for="item in carousel_img_src_list" :key="item.index">
-          <img :src="item.url"/>
+          <img class="carousel-image" :src="item.url"/>
         </el-carousel-item>
       </el-carousel>
+      <div class="container">
+        <div id="main_char">
+          <h3>关键特征</h3>
+          <div class="row" v-for="item in feature_small" :key="item.index">
+            <div class="col-md-3 col-xs-3 col-sm-3">
+              <img :src="item.url" class="img-responsive img-thumbnail">
+              <p>{{item.intro}}</p>
+            </div>
+          </div>
+        </div>
+        <div class="row" v-for="item in feature_large" :key="item.index" style="margin-top: 10px;">
+          <div class="col-md-6 col-xs-6 col-sm-6">
+            <img :src="item.url" class="img-responsive img-thumbnail">
+            <p>{{item.intro}}</p>
+          </div>
+        </div>
+      </div>
     </el-main>
     <el-footer></el-footer>
   </el-container>
@@ -21,30 +38,55 @@ export default {
   components: {IndexHeader},
   data: function () {
     return {
+      screenWidth: document.body.clientWidth,
       carousel_img_src_list: [
-        {url: '../../assets/image/index/index_ad_01.jpg', index: 1},
-        {url: '../../assets/image/index/index_ad_02.jpg', index: 2},
-        {url: '../../assets/image/index/index_ad_03.jpg', index: 3}
+        {url: require('../../assets/image/index/index_ad_01.jpg'), index: 1},
+        {url: require('../../assets/image/index/index_ad_02.jpg'), index: 2},
+        {url: require('../../assets/image/index/index_ad_03.jpg'), index: 3}
+      ],
+      feature_small: [
+        {url: require('../../assets/image/index/index_intro_1.png'), index: 1, intro: '您都可以轻松管理自己的班课，管理学生、发送通知、分享资源、布置批改作业、组织讨论答疑、开展教学互动。'},
+        {url: require('../../assets/image/index/index_intro_2.png'), index: 2, intro: '任何普通教室的课堂现场或课外，您都可以随即开展投票问卷、头脑风暴、作品分享、计时答题等互动教学活动。即刻反馈，即刻点评。'},
+        {url: require('../../assets/image/index/index_intro_3.png'), index: 3, intro: '您发布的所有课程信息、学习要求、课件、微视频等学习资源都可以即时传递到学生设备上，从而让学生设备从此变成学习工具，不再只是社交、游戏…'}
+      ],
+      feature_large: [
+        {url: require('../../assets/image/index/index_intro_4.png'), index: 1, intro: '配套交互式数字教材,可以实现对每位学生学习进度跟踪和学习成效评价，学期末教师可以得到每位学生的数字教材学习评估报告。'},
+        {url: require('../../assets/image/index/index_intro_5.png'), index: 2, intro: '对教师和学生全部免费。'}
       ]
+    }
+  },
+  mounted () {
+    const that = this
+    window.onresize = () => {
+      return (() => {
+        window.screenWidth = document.body.clientWidth
+        that.screenWidth = window.screenWidth
+      })()
+    }
+  },
+  watch: {
+    screenWidth (val) {
+      if (!this.timer) {
+        this.screenWidth = val
+        this.timer = true
+        let that = this
+        setTimeout(function () {
+          // that.screenWidth = that.$store.state.canvasWidth
+          console.log(that.screenWidth)
+          that.init()
+          that.timer = false
+        }, 400)
+      }
     }
   }
 }
 </script>
 
-<style scoped>
-  .el-carousel__item h3 {
-    color: #475669;
-    font-size: 14px;
-    opacity: 0.75;
-    line-height: 200px;
-    margin: 0;
-  }
-
-  .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-  }
-
-  .el-carousel__item:nth-child(2n+1) {
-    background-color: #d3dce6;
-  }
+<style lang="stylus" rel="stylesheet/stylus">
+  .carousel-image
+    display: inline-block
+    max-width: 100%
+  .el-row:before
+    content: " "
+    margin-top 2px
 </style>
--- src/components/index/IndexHeader.vue
+++ src/components/index/IndexHeader.vue
@@ -1,15 +1,15 @@
 <template>
-  <el-row align="middle">
+  <el-row>
     <el-col :xs="4" :sm="2">
-      <a href="#"><img class="logo-image" src="../../assets/image/logo.png" height="60"></a>
+      <a href="#"><img src="../../assets/image/logo.png" height="60"></a>
     </el-col>
     <el-col :xs="20" :sm="22">
-      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+      <el-menu :default-active="activeIndex" mode="horizontal">
         <el-menu-item index="1"><a href="">首页</a></el-menu-item>
-        <el-menu-item index="2"><a href="">关键特征</a></el-menu-item>
+        <el-menu-item class="hidden-xs-only" index="2"><a href="">关键特征</a></el-menu-item>
         <el-menu-item index="3"><a href="">如何使用</a></el-menu-item>
-        <el-menu-item index="4"><a href="">学校服务</a></el-menu-item>
-        <el-menu-item index="5"><a href="">用户反馈</a></el-menu-item>
+        <el-menu-item class="hidden-xs-only" index="4"><a href="">学校服务</a></el-menu-item>
+        <el-menu-item class="hidden-xs-only" index="5"><a href="">用户反馈</a></el-menu-item>
         <el-menu-item index="6"><a href="">登录</a></el-menu-item>
       </el-menu>
     </el-col>
@@ -22,11 +22,6 @@ export default {
   data () {
     return {
       activeIndex: '1'
-    }
-  },
-  methods: {
-    handleSelect (key, keyPath) {
-      console.log(key, keyPath)
     }
   }
 }
